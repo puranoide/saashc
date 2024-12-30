1e8743d709a6a916c307c33ad7e8a5580cc98623
puranoide <acostasanchezangelgabriel@gmail.com>
2024-12-29 20:46:46 -0500

    lista de historias por paciente y vista de agregado de historia

--- app/inicio.php
+++ app/inicio.php
@@ -86,10 +86,27 @@ elseif ($dni != null) :
             return "error al conectarse a la base de datos,contacta con soporte";
         }
     }
+    function buscarHistorial($con, $idPaciente)
+    {
+        $historialQuery = "SELECT*FROM historia WHERE idPaciente='$idPaciente'";
+        $historialResult = $con->query($historialQuery);
+        if ($historialResult) {
+            if (mysqli_num_rows($historialResult) > 0) {
+                $historial = mysqli_fetch_all($historialResult, MYSQLI_ASSOC);
+                return $historial;
+            } else {
+                return "no se encuentra historia,intente de nuevo";
+            }
+        } else {
+            return "error al conectarse a la base de datos,contacta con soporte";
+        }
+    }
+ 
 
     
     $paciente = buscarPaciente($con, $dni);
-
+    print_r($paciente);
+    $historias= buscarHistorial($con, $paciente['id']);
     if (is_array($paciente)):
     ?>
         <!DOCTYPE html>
@@ -139,6 +156,30 @@ elseif ($dni != null) :
                     <button class="btn btn-primary mt-3" id="agregar" onclick="window.location.href='managementPaciente.php?dni=<?= $paciente['dni'] ?>&action=update' ">Actualizar cliente</button>
                 </div>
 
+                
+               <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">id</th>
+                            <th scope="col">fecha</th>
+                            <th scope="col">diagnostico</th>
+                            <th scope="col">tratamiento</th>
+                            <th scope="col">acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($historias as $historia) : ?>
+                            <tr>
+                                <th scope="row"><?= $historia['id'] ?></th>
+                                <td><?= $historia['dateCreated'] ?></td>
+                                <td><?= $historia['antecedente'] ?></td>
+                                <td><?= $historia['receta'] ?></td>
+                                <td>
+                                    <button class="btn btn-primary" onclick="window.location.href='managementHistoria.php?id=<?= $historia['id'] ?>' ">editar</button>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                </table>
 
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
         </body>
---
+++ app/managementHistoria.php
@@ -0,0 +1,89 @@
+<?php
+
+session_start();
+
+$dni = isset($_GET['dni']) ? $_GET['dni'] : null;
+$action = isset($_GET['action']) ? $_GET['action'] : null;
+
+
+if (is_null($action) && !is_null($dni)) :
+    include_once "../config/db.php";
+    function buscarPaciente($con, $dni)
+    {
+        $pacienteQuery = "SELECT*FROM paciente WHERE dni='$dni'";
+        $pacienteResult = $con->query($pacienteQuery);
+        if ($pacienteResult) {
+            if (mysqli_num_rows($pacienteResult) > 0) {
+                $paciente = mysqli_fetch_assoc($pacienteResult);
+                return $paciente;
+            } else {
+                return "no se encuentra paciente,intente de nuevo";
+            }
+        } else {
+            return "error al conectarse a la base de datos,contacta con soporte";
+        }
+    }
+
+
+    $paciente = buscarPaciente($con, $dni);
+    print_r($paciente);
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <title>Agregar paciente</title>
+    </head>
+
+    <body>
+
+        <h1 class="text-center mt-5">Nueva historia</h1>
+        <p class="text-center mt-2"> paciente:<?= $paciente['nombreyapellido'] ?><br> dni:<?= $paciente['dni'] ?> </p>
+
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-6 offset-3">
+                    <div class="card">
+                        <div class="card-header">
+                            Agregar Paciente
+                        </div>
+                        <div class="card-body">
+                            <form action="../logic/historia.php" method="POST">
+                                <input type="hidden" name="action" value="add">
+                                <input type="hidden" name="dni" value="<?= $paciente['id'] ?>">
+                                <div class="mb-3">
+                                    <label for="exampleInputEmail1" class="form-label">Antecendente</label>
+                                    
+                                    <textarea name="antecendente" id="" class="form-control"></textarea>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleInputEmail1" class="form-label">receta</label>
+                                    <textarea name="receta" id="" class="form-control"></textarea>
+                                </div>
+                                <button type="submit" class="btn btn-primary">agregar</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    </body>
+
+    </html>
+<?php
+else: ?>
+    editar historia
+<?php endif;
+
+
+?>
